56e194ee826c4ae7a8a21a21166a4730aae08739
rithikha <rithikha.rajamohan@gmail.com>
2023-08-09 20:02:04 -0400

    updated read and add for edit functionality

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,9 @@ fn add_task(task: Json<Task<'_>>) -> &'static str {
                     .create(true)
                     .open("tasks.txt")
                     .expect("unable to access tasks.txt");   
-    let task_item_string = format!("{}\n", task.item);
+    let reader = BufReader::new(&tasks);
+    let id = reader.lines().count();
+    let task_item_string = format!("{},{}\n", id, task.item);
     let task_item_bytes = task_item_string.as_bytes();
     tasks.write(task_item_bytes).expect("unable to write to tasks.txt");
     "Task added succesfully"
@@ -41,11 +43,17 @@ fn read_tasks() -> Json<Vec<String>> {
                 .create(true)
                 .open("tasks.txt")
                 .expect("unable to access tasks.txt");
-            let reader = BufReader::new(tasks);
-            Json(reader.lines()
-                        .map(|line| line.expect("unable to read line"))
-                        .collect())
+    let reader = BufReader::new(tasks);
+    Json(reader.lines()
+            .map(|line| {
+                let line_string: String = line.expect("unable to read line"))
+                let line_pieces: Vec<&str> = line_string.split(",").collect();
+                line_pieces[1].to_string()
+            })
+            .collect())
 }
+
+
 
 
 
