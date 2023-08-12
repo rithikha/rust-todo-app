76c7fd6b271a31f6ffe3cb32ee9ffd3a84fd6e4f
rithikha <rithikha.rajamohan@gmail.com>
2023-08-12 12:09:03 -0400

    edit task

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -10,25 +11,25 @@ fn rocket() -> _ {
     rocket::build().mount("/", routes![index])
 }
 
-use std::{fs::{OpenOptions}, io::{Write}};
-use rocket::serde::{Deserialize, Serialize, json::Json};
+use std::{ fs::{ OpenOptions }, io::{ Write } };
+use rocket::serde::{ Deserialize, Serialize, json::Json };
 use std::io::BufReader;
 use std::io::prelude::*;
 
 #[derive(Deserialize, Serialize)]
 #[serde(crate = "rocket::serde")]
 struct Task<'r> {
-    item: &'r str
+    item: &'r str,
 }
 
-#[post("/addtask", data="<task>")]
+#[post("/addtask", data = "<task>")]
 fn add_task(task: Json<Task<'_>>) -> &'static str {
     let mut tasks = OpenOptions::new()
-                    .read(true)
-                    .append(true)
-                    .create(true)
-                    .open("tasks.txt")
-                    .expect("unable to access tasks.txt");   
+        .read(true)
+        .append(true)
+        .create(true)
+        .open("tasks.txt")
+        .expect("unable to access tasks.txt");
     let reader = BufReader::new(&tasks);
     let id = reader.lines().count();
     let task_item_string = format!("{},{}\n", id, task.item);
@@ -36,60 +37,64 @@ fn add_task(task: Json<Task<'_>>) -> &'static str {
     tasks.write(task_item_bytes).expect("unable to write to tasks.txt");
     "Task added succesfully"
 }
- 
+
 #[get("/readtasks")]
 fn read_tasks() -> Json<Vec<String>> {
     let tasks = OpenOptions::new()
-                .read(true)
-                .append(true)
-                .create(true)
-                .open("tasks.txt")
-                .expect("unable to access tasks.txt");
+        .read(true)
+        .append(true)
+        .create(true)
+        .open("tasks.txt")
+        .expect("unable to access tasks.txt");
     let reader = BufReader::new(tasks);
-    Json(reader.lines()
+    Json(
+        reader
+            .lines()
             .map(|line| {
                 let line_string: String = line.expect("unable to read line");
                 let line_pieces: Vec<&str> = line_string.split(",").collect();
                 line_pieces[1].to_string()
             })
-            .collect())
+            .collect()
+    )
 }
 
 struct TaskUpdate<'r> {
     id: u8,
-    item: &'r str
+    item: &'r str,
 }
 
-#[put("/edittask", data="<task_update>")]
+#[put("/edittask", data = "<task_update>")]
 fn edit_task(task_update: Json<TaskUpdate<'_>>) -> &'static str {
     let tasks = OpenOptions::new()
-                .read(true)
-                .append(true)
-                .create(true)
-                .open("tasks.text")
-                .expect("unable to access tasks.txt");
+        .read(true)
+        .append(true)
+        .create(true)
+        .open("tasks.txt")
+        .expect("unable to access tasks.txt");
     let mut temp = OpenOptions::new()
-                .create(true)
-                .write(true)
-                .truncate(true)
-                .open("temp.txt")
-                .expect("unable to access temp.txt");
+        .create(true)
+        .write(true)
+        .truncate(true)
+        .open("temp.txt")
+        .expect("unable to access temp.txt");
 
     let reader = BufReader::new(tasks);
-
     for line in reader.lines() {
         let line_string: String = line.expect("could not read line");
         let line_pieces: Vec<&str> = line_string.split(",").collect();
-        
-        if line_pieces[0].parse::<u8>().expect("unable to parse id") != task_id.id {
+
+        if line_pieces[0].parse::<u8>().expect("unable to parse id as u8") == task_update.id {
+            let task_items: [&str; 2] = [line_pieces[0], task_update.item];
+            let task = format!("{}\n", task_items.join(","));
+            temp.write(task.as_bytes()).expect("could not write to temp file");
+        } else {
             let task = format!("{}\n", line_string);
             temp.write(task.as_bytes()).expect("could not write to temp file");
         }
     }
-    
+
     std::fs::remove_file("tasks.txt").expect("unable to remove tasks.txt");
     std::fs::rename("temp.txt", "tasks.txt").expect("unable to rename temp.txt");
-    "Task deleted succesfully"
+    "Task updated succesfully"
 }
-
-
